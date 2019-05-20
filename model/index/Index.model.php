844654a0c4f8038548398c7713608ab2640f269b
sroehrl <sroehrl@eagleproductivity.com>
2019-05-20 09:13:32 -0400

    integrating models

---
+++ model/index/Index.model.php
@@ -0,0 +1,12 @@
+<?php
+namespace Neoan3\Model;
+class IndexModel {
+
+    static function first($ask){
+        if(!empty($ask)){
+            return $ask[0];
+        } else {
+            return [];
+        }
+    }
+}
