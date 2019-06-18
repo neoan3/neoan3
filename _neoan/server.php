b94edefd0092f879cfe13fae98f8ac503d658214
sroehrl <sroehrl@eagleproductivity.com>
2019-06-18 11:53:58 -0400

    basic routing-support for built-in & nginx

--- _neoan/server.php
+++ _neoan/server.php
@@ -1,16 +1,23 @@
 <?php
 $queryString = $_SERVER["REQUEST_URI"];
-$_GET['action'] = preg_replace('/^\//','',$queryString);
+
 $appLevel = dirname(__DIR__);
 
-if (file_exists($appLevel.$queryString)) {
-    return false;
+function writeAction($subtract = '') {
+    $request = preg_replace('/^\//', '', strtok($_SERVER["REQUEST_URI"], '?'));
+    $_GET['action'] = substr($request, strlen($subtract));
+}
+
+if(file_exists($appLevel . $queryString) && !is_dir($appLevel . $queryString)) {
+    echo file_get_contents($appLevel . $queryString);
 } elseif(preg_match('/api.v1\/(.*)$/', $queryString)) {
     require_once $appLevel . '/_neoan/base/Api.php';
-}elseif(preg_match('/file.serve\/(.*)$/', $queryString)) {
+} elseif(preg_match('/serve.file\/(.*)$/', $queryString)) {
+    writeAction('/serve.file/');
     require_once $appLevel . '/_neoan/base/FileServe.php';
-} elseif (preg_match('/^node_modules\/(.*)$/', $queryString)){
+} elseif(preg_match('/^node_modules\/(.*)$/', $queryString)) {
     include $appLevel . DIRECTORY_SEPARATOR . '_neoan/base/Node.php';
 } else {
+    writeAction();
     include $appLevel . DIRECTORY_SEPARATOR . 'index.php';
 }
