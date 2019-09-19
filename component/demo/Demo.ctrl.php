1eb7be79265082cba981eeaca56917c0d44a7063
neoan <sroehrl@gmail.com>
2019-09-19 08:56:09 -0400

    "Beautify" demo, clean up dependencies

--- component/demo/Demo.ctrl.php
+++ component/demo/Demo.ctrl.php
@@ -8,8 +8,9 @@ class Demo extends Unicore
 {
     function init()
     {
+        $info = json_decode(file_get_contents(path.'/composer.json'),true);
         $this->uni('demo')
-             ->hook('main', 'demo')
+             ->hook('main', 'demo', $info)
              ->output();
     }
 }
