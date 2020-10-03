38636c2aea22e60fd72ebf58d60abb43ae9c740f
neoan <stefan.roehrl@gmail.com>
2020-10-02 21:03:04 -0400

    Fix mocking of database

--- component/endpoint/Endpoint.ctrl.php
+++ component/endpoint/Endpoint.ctrl.php
@@ -13,6 +13,25 @@ use Neoan3\Frame\Demo;
 
 class Endpoint extends Demo{
 
+    /**
+     * Get to know how API endpoints work!
+     *
+     * The following function accounts for the endpoints:
+     *
+     * GET /api.v1/endpoint
+     * GET /api.v1/endpoint?any-key=any-value
+     * GET /api.v1/endpoint/{userType}
+     * GET /api.v1/endpoint/{userType}?any-key=any=value
+     * GET /api.v1/endpoint/{userType}/{id}
+     * GET /api.v1/endpoint/{userType}/{id}?any-key=any-value
+     *
+     * try them out by visiting these combinations in your browser
+     *
+     * @param string $userType
+     * @param null $id
+     * @param array $params
+     * @return array
+     */
     function getEndpoint($userType='self', $id=null, $params=[]): array
     {
         return array_merge([
